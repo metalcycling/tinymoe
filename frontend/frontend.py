91e6c59100a60288bf6479ed0c0cbc1652ddacdf
Pedro D. Bello-Maldonado <metalcycling@gmail.com>
2026-04-10 19:03:10 -0400

    Use trained model to compute projection

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -4,41 +4,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from data.functions import polynomial
+from data.loader import DEFAULT_COEFFICIENTS
 from data.projection import find_projection
+from src.inference import load_model, infer
+
+# %% Load model
+
+model = load_model()
 
 # %% Setup figure
 
 fig, ax = plt.subplots(figsize=(12, 8))
 ax.set_aspect("equal")
 ax.grid(True, alpha=0.3)
-ax.set_title("Move mouse to update the point and show the projection to the curve")
+ax.set_title("Move mouse to see predicted projection and expert assignment")
 
-# %% Define polynomial function
+# %% Plot expert polynomials
 
-coeff = np.array([0.5, 0.5, 0.0, 0.0])
+colors = ["tab:blue", "tab:orange", "tab:green", "tab:red"]
+x_curve = np.linspace(-3.0, 3.0, 300)
 
-x = np.linspace(-2.0, 2.0)
-y = polynomial(x, coeff)
-
-ax.plot(x, y, "b-", linewidth=2, label="polynomial")
+for j, coeff in enumerate(DEFAULT_COEFFICIENTS):
+    y_curve = polynomial(x_curve, coeff)
+    ax.plot(x_curve, y_curve, color=colors[j], linewidth=2, label=f"Expert {j}")
 
 # %% Plot initial point
 
-point_drawing, = ax.plot([], [], "ro", markersize=10, zorder=5, label="point")
+point_drawing, = ax.plot([], [], "ko", markersize=10, zorder=5, label="point")
 
-# %% Projection point
+# %% Model projection
 
-projection_drawing, = ax.plot([], [], "gs", markersize=8, zorder=5, label="projection")
-projection = (0.0, 0.0)
+model_proj_drawing, = ax.plot([], [], "s", markersize=8, zorder=5, label="model projection")
+model_proj_line, = ax.plot([], [], "--", alpha=0.5, linewidth=1)
 
-# %% Line from point to projection
+# %% Analytic projection
 
-projection_line, = ax.plot([], [], "g--", alpha=0.5, linewidth=1)
+analytic_proj_drawing, = ax.plot([], [], "^", markersize=8, zorder=5, label="analytic projection")
+analytic_proj_line, = ax.plot([], [], ":", alpha=0.5, linewidth=1)
 
 ax.legend(loc="upper right")
-ax.set_xlim(xmin=x[0], xmax=x[-1])
+ax.set_xlim(-3, 3)
+ax.set_ylim(-3, 3)
 
 # %% Update event
+
+analytic_guess = {}
 
 def on_move(event):
     """
@@ -47,22 +57,33 @@ def on_move(event):
     if event.inaxes != ax:
         return
 
-    point = event.xdata, event.ydata
+    point = (event.xdata, event.ydata)
 
-    # Update the red point
+    # Model inference
+
+    projections, expert_indices = infer(model, [point])
+
+    model_proj = projections[0]
+    expert = expert_indices[0]
+    color = colors[expert]
 
     point_drawing.set_data([point[0]], [point[1]])
+    model_proj_drawing.set_data([model_proj[0]], [model_proj[1]])
+    model_proj_drawing.set_color(color)
+    model_proj_line.set_data([point[0], model_proj[0]], [point[1], model_proj[1]])
+    model_proj_line.set_color(color)
 
-    # Recompute projection point
+    # Analytic projection on the same expert's polynomial
 
-    global projection
+    coeff = DEFAULT_COEFFICIENTS[expert]
+    guess = analytic_guess.get(expert, point[0])
+    ax_proj, ay_proj = find_projection(point, coeff, guess=guess)
+    analytic_guess[expert] = ax_proj
 
-    projection = find_projection(point, coeff, guess=projection[0])
-    projection_drawing.set_data([projection[0]], [projection[1]])
-
-    # Update lines from point to projection
-
-    projection_line.set_data([point[0], projection[0]], [point[1], projection[1]])
+    analytic_proj_drawing.set_data([ax_proj], [ay_proj])
+    analytic_proj_drawing.set_color(color)
+    analytic_proj_line.set_data([point[0], ax_proj], [point[1], ay_proj])
+    analytic_proj_line.set_color(color)
 
     fig.canvas.draw_idle()
 
